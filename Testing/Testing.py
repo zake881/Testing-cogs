7ef50a3b367c7a069933880f12589578dadb55d5
zake881 <40245714+zake881@users.noreply.github.com>
2018-07-04 23:28:20 +0100

    ping

--- Testing/Testing.py
+++ Testing/Testing.py
@@ -16,11 +16,20 @@ class Jakestest:
         """This is Jakes way of slowly learning kek"""
         await self.bot.say("this is all it is")
         
-    @commands.command(pass_context=True)
-    async def ping(ctx):
-        embed = discord.Embed(title="Pong! :ping_pong:")
-        await bot.say(embed=embed)
+class Ping:
+    """a semi-actual ping"""
 
+    def __init__(self,bot):
+        self.bot = bot
+        
+
+    @commands.command(pass_context=True)
+    async def pingt(self,ctx):
+        """pseudo-ping time"""
+        channel = ctx.message.channel
+        t1 = time.perf_counter()
+        t2 = time.perf_counter()
+        await self.bot.say("pseudo-ping: {}ms".format(round((t2-t1)*1000)))
 
     
 def setup(bot):
