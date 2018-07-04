03b3614c4c6f9a58179246569e4f4109a24df4df
zake881 <40245714+zake881@users.noreply.github.com>
2018-07-04 23:15:18 +0100

    Update Testing.py

--- Testing/Testing.py
+++ Testing/Testing.py
@@ -15,6 +15,10 @@ class Jakestest:
     async def Jake2(self, ctx):
         """This is Jakes way of slowly learning kek"""
         await self.bot.say("this is all it is")
+        
+    @commands.command()
+    async def ping(ctx):
+    return await ctx.send('Pong! {0}'.format(round(bot.latency, 1))
 
 def setup(bot):
     bot.add_cog(Jakestest(bot))
